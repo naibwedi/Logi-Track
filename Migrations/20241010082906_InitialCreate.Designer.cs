18aa0aab0e81a05fc9d8cf34cf3471339528314e
Evan Belal <evanb@uia.no>
2024-10-10 13:25:46 +0200

    homepage

--- Migrations/20241010082906_InitialCreate.Designer.cs
+++ Migrations/20241010082906_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using logirack.Data;
 
@@ -10,9 +11,11 @@ using logirack.Data;
 namespace logirack.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241010082906_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
