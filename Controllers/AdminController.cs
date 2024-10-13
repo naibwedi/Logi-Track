095f135524bbd5ee2e7daa05ffbaafc39f55839a
GH4ZII <ghaazii59@hotmail.com>
2024-10-13 20:52:42 +0200

    Test

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -8,4 +8,28 @@ public class AdminController : Controller
     {
         return View();
     }
+
+    public IActionResult ManageDrivers()
+    {
+        return View();
+    }
+
+    public IActionResult AddNewTrip()
+    {
+        return View();
+    }
+
+
+    public IActionResult InvoiceGeneration()
+    {
+        return View();
+    }
+
+    public IActionResult PaymentManagement()
+    {
+        return View();
+    }
+    
+    
+    
 }
