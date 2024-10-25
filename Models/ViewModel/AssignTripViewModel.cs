efaa72f24cd7fd2b12c5cb5720fcbc311e62c348
Ilyass Bouchabti <ilyassb@student.uia.no>
2024-10-25 07:04:47 +0200

    Enhance DriverTrip model and AssignTripViewModel
    - Created AssignTripViewModel to facilitate trip assignment functionality.
    - Added required fields and validation for driver selection in AssignTripViewModel.
    - Updated DriverTrip model to include nullable `AssignedByAdminId` for better flexibility.
    - Added `Status` field to DriverTrip to track the trip's progress.
    - Retained fields for tracking driver payments, trip assignment, and completion dates.
    - Improved model consistency and expanded functionality to support trip assignment features.

---
+++ Models/ViewModel/AssignTripViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace logirack.Models.ViewModel;
+
+public class AssignTripViewModel
+{
+    public int TripId { get; set; }
+    public string FromCity { get; set; }
+    public string ToCity { get; set; }
+    public double Distance { get; set; }
+    public double Weight { get; set; }
+    
+    [Required(ErrorMessage = "please select a driver")]
+    public string DriverId { get; set; }
+    
+    public List<Driver> AvailableDrivers { get; set; }
+    
+}
