c34ced8fc9c42d1ac30cd2b0586bfeed6fba3457
Ilyass Bouchabti <ilyassb@student.uia.no>
2024-10-25 02:37:58 +0200

    Updating the Basic View for dev only

--- Models/ViewModel/SetPriceViewModel.cs
+++ Models/ViewModel/SetPriceViewModel.cs
@@ -5,9 +5,9 @@ namespace logirack.Models.ViewModel;
 public class SetPriceViewModel
 {
     public int TripId { get; set; }
-    public string CustomerName { get; set; }
-    public string FromCity { get; set; }
-    public string ToCity { get; set; }
+    public string? CustomerName { get; set; }
+    public string? FromCity { get; set; }
+    public string? ToCity { get; set; }
     public double Distance { get; set; }
     public double Weight { get; set; }
     public double EstimatedPrice { get; set; }
@@ -16,8 +16,5 @@ public class SetPriceViewModel
     [Range(0,double.MaxValue,ErrorMessage = "Price mute be positive number")]
     [DataType(DataType.Currency)]
     public double AdminPrice { get; set; }
-    [Required]
-    [Display(Name= "Price Justification")]
-    [StringLength(500, ErrorMessage = "Price mute be longer than 500 characters")]
-    public string PriceJustification { get; set; }
+    
 }
